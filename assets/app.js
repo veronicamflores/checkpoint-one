128f7ce9f6c8f5d2efce51beef3324fa13b756ba
Veronica Flores <veronica.marie.flores@gmail.com>
2018-08-03 16:57:35 -0600

    push the semi working code yay

--- assets/app.js
+++ assets/app.js
@@ -6,40 +6,38 @@ let target = {
 
 let items = {
     fire:{name: 'Fire Hands', modifier: 2, description: 'its a slap'},
-    water: {name: 'Water Waves', modifier: 10, description: 'its a punch'},
-    lava: {name: 'Lava You Pretty Dead', modifier: 20, description: 'its a kick'
+    water: {name: 'Water Waves', modifier: 5, description: 'its a punch'},
+    lava: {name: 'Lava You Pretty Dead', modifier: 10, description: 'its a kick'
     }
 }
-function getFire(){
-    target.items.push(items.fire)
+function addMods(arr){
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        total = total + arr[1].modifier
+    }
+    return total
 }
-function getWater(){
-    target.items.push(items.water)
-}
-function getLava(){
-    target.items.push(items.lava)
+function getItem(mod){
+    if(mod == 'fire'){
+        target.items.push(items.fire);
+    }else if(mod == 'water'){
+        target.items.push(items.water);
+    }else{
+        target.items.push(items.lava);    } 
 }
 
-function damage(attack){
-    if(attack == 'slap'){
-        target.health = target.health - 1;
-    }else if(attack == 'punch'){
-        target.health = target.health - 5;
-    }else if(attack == 'kick'){
-    target.health = target.health - 10;
-    }
+function slap(){
+    target.health = target.health - 1;
     target.hits++;
     upDate();
 }
 function punch(){
-
-   
+    target.health = target.health - 5;
     target.hits++;
     upDate();
 }
 function kick(){
-
-   
+    target.health = target.health - 10;
     target.hits++;
     upDate();
 }
@@ -49,12 +47,6 @@ function upDate(){
     document.getElementById('hits').innerText = target.hits;
 }
 
-function addMods(arr){
-    let total = 0;
-    for (let i = 0; i < arr.length; i++) {
-        total = total + arr[1].modifier
-    }
-    return total
-}
+
 
 upDate();
